12836728e69e0f7dc3e68dd028b5315169a8c0f1
lupita21134 <119145434+lupita21134@users.noreply.github.com>
2023-05-23 16:25:38 -0600

    Update send.php

--- send.php
+++ send.php
@@ -3,18 +3,18 @@
 
 //varible setting
  $name= $_REQUEST['name'];
- $email = $_REQUEST['Email'];
+ $email = $_REQUEST['asistencia'];
  $message = $_REQUEST['comment'];
  $subject = "Message from Contact form !";
 
  $to ="lupitaksb721@gmail.com";  // change receiving email id 
 
- $content = "Name : ". $name. "\r\nContact email : ". $email. "\r\n \r\ncomment : \r\n \r\n".$comment ; // name [break] email [break] message
+ $content = "Name : ". $name. "\r\nContact email : ". $asistencia. "\r\n \r\ncomment : \r\n \r\n".$comment ; // name [break] email [break] message
 
 
 
 // check input fields
-if ( empty($name)|| empty($email)|| empty($comment))
+if ( empty($name)|| empty($asistencia)|| empty($comment))
 {
 echo"<script type='text/javascript'>alert('Please fill all correct');
     window.history.go(-1);
